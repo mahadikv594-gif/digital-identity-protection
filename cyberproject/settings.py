f4dbcbcad034f739744ca5b4737d0684a3617c53
apple <apple@apples-MacBook-Pro.local>
2026-06-25 12:43:49 +0530

    FIX DEBUG and ALLOWED_HOSTS

--- cyberproject/settings.py
+++ cyberproject/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uab7v&uq0pm41s!s0ytzx@!!^m23obmjxn9gg#(&dkndohj4j#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
